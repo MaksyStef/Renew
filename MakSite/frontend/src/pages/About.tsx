4eeab7a1d22a9132c2e41e9a73e4c507a46d298c
MaksyStef <maksistef@gmail.com>
2026-02-25 04:11:40 +0100

    Redo the About page about_1 grid, got rid of the acrylic panel

--- MakSite/frontend/src/pages/About.tsx
+++ MakSite/frontend/src/pages/About.tsx
@@ -1,24 +1,41 @@
-import type { JSX } from 'react'
-import { Link } from 'react-router-dom'
+import type { JSX } from "react"
+import { Link } from "react-router-dom"
 
-import Footer from '../components/Footer.tsx'
-import PageWrapper from '../components/PageWrapper.tsx'
+import Footer from "../components/Footer.tsx"
+import PageWrapper from "../components/PageWrapper.tsx"
 
-import '../styles/about.scss'
+import "../styles/about.scss"
 
 export default function About(): JSX.Element {
   return (
     <>
       <PageWrapper className="page-wrapper_snap">
-        <div className="about_1 w-75 vh-100 d-flex flex-column justify-content-center align-items-center">
-          <h1 className="text-center display-1"><strong>Maksym Stefaniv</strong></h1>
-          <h2 className="text-center"><strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis dolorum consectetur odit voluptates porro adipisci ut odio vitae temporibus quibusdam, accusantium deserunt totam hic similique, provident ab nostrum, magni repudiandae!</strong></h2>
-          <div className="container container_photos position-absolute w-100 h-100 bottom-0">
-            <div className="d-inline container-sm container-sm_1 position-absolute"><img className='w-100 h-100' src="https://placehold.co/600x400"/></div>
-            <div className="d-inline container-sm container-sm_2 position-absolute"><img className='w-100 h-100' src="https://placehold.co/400x600"/></div>
-            <div className="d-inline container-sm container-sm_3 position-absolute"><img className='w-100 h-100' src="https://placehold.co/400x600"/></div>
-            <div className="d-inline container-sm container-sm_4 position-absolute"><img className='w-100 h-100' src="https://placehold.co/400x400"/></div>
-            <div className="d-inline container-sm container-sm_5 position-absolute"><img className='w-100 h-100' src="https://placehold.co/600x400"/></div>
+        <div className="about_1  w-75 vh-100 d-flex flex-column justify-content-center align-items-center">
+          <div className="container w-100 h-100">
+            <div className="about_grid d-grid w-100 h-100">
+              <div className="w-100 h-100 d-flex image_center">
+                <img src="https://placehold.co/400x400"/>
+              </div>
+              <div className="w-100 h-100 d-flex image_top">
+                <img src="https://placehold.co/600x400"/>
+              </div>
+              <div className="w-100 h-100 d-flex  image_left">
+                <img src="https://placehold.co/400x600"/>
+              </div>
+              <div className="w-100 h-100 d-flex image_bottom">
+                <img src="https://placehold.co/600x400"/>
+              </div>
+              <div className="w-100 h-100 d-flex image_right">
+                <img src="https://placehold.co/400x600"/>
+              </div>
+              <div className="text_1">
+                <h1 className="display-4 fs-2"><strong>About Me</strong></h1>
+                <h2 className="display-6 fs-4"><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem soluta facilis inventore quibusdam repellendus accusamus voluptate beatae vel cupiditate impedit atque, at mollitia</strong></h2>
+              </div>
+              <div className="text_2 align-bottom pt-4 text-end">
+                <h2 className="display-6 fs-4"><strong>Aliquid voluptas iure officia ea officiis necessitatibus saepe corporis sapiente aperiam laudantium in accusamus, iusto, doloremque reprehenderit dolor dolore iusto accusamus ipsa</strong></h2>
+              </div>
+            </div> 
           </div>
         </div>
         <div className="about_2 w-75 vh-100 d-flex flex-column justify-content-center align-items-center">
