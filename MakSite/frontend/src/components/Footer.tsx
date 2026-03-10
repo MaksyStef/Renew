097b34d5eb6d7ab94ee60e46cddf268be41488e0
MaksyStef <maksistef@gmail.com>
2026-03-10 03:50:16 +0100

    dynamic footer className implementation

--- MakSite/frontend/src/components/Footer.tsx
+++ MakSite/frontend/src/components/Footer.tsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Footer(props: any): JSX.Element {
   return (
-    <footer {...props} className="bg-gray-800 text-white py-4">
+    <footer {...props} 
+    className={
+        // if classname is provided, add it to the default classname, otherwise just use the default classname
+        props.className ? props.className + " bg-gray-800 text-white py-4" : "bg-gray-800 text-white py-4"
+    }>
+    
       <div className="container mx-auto text-center">
         <p>&copy; 2026 MakSite. All rights reserved.</p>
       </div>
