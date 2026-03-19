7ab0bba8e26a8744bf7d1f0357f9a79c4e3ab325
MaksyStef <maksistef@gmail.com>
2026-03-19 06:18:51 +0100

    add Media model to admin

--- MakSite/api/admin.py
+++ MakSite/api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Media
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
     pass
     
+class MediaAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Media, MediaAdmin)
