590bf1c7c3daa1cc6e978e7c0fb164321e8b7df7
MaksyStef <maksistef@gmail.com>
2026-03-19 02:01:10 +0100

    Add separator property to Footer component

--- MakSite/frontend/src/components/Footer.tsx
+++ MakSite/frontend/src/components/Footer.tsx
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 
 export default function Footer(props: any): JSX.Element {
   return (
-    <footer {...props} 
-    className={
-        // if classname is provided, add it to the default classname, otherwise just use the default classname
-        props.className ? props.className + " bg-gray-800 text-white py-4" : "bg-gray-800 text-white py-4"
-    }>
-    
-      <div className="container mx-auto text-center">
-        <p>&copy; 2026 MakSite. All rights reserved.</p>
-      </div>
-    </footer>
+    <>
+      { props.separator ? <div style={{flex: "1 1 auto"}} className="separator" /> : null }
+      <footer {...props} 
+      className={
+          // if classname is provided, add it to the default classname, otherwise just use the default classname
+          props.className ? props.className + " bg-gray-800 text-white py-4" : "bg-gray-800 text-white py-4"
+      }>
+      
+        <div className="container mx-auto text-center">
+          <p>&copy; 2026 MakSite. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
   );
 }
