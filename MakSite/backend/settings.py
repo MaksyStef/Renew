1bc8e8c8bc89995a74ba729b57f3afe53d598b62
MaksyStef <maksistef@gmail.com>
2026-03-19 02:30:19 +0100

    os.path.join(...) -> BASE_DIR.joinpath(...)

--- MakSite/backend/settings.py
+++ MakSite/backend/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR.joinpath('static')
 MEDIA_URL  = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 
 # DJANGO REST FRAMEWORK settings
